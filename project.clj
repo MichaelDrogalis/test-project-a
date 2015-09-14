fa7675f1cb89f3ec0badcfe244abd97b1d9867a9
Michael Drogalis <mjd3089@rit.edu>
2015-09-14 09:21:51 -0700

    Requires a Voom dep.

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 ^{:voom {:repo "git@github.com:clojure/core.async.git" :branch "master"}}
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]])
